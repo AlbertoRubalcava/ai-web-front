bfe7f420d4b3607bf1ba484193860acf73b474d6
Alberto Rubalcava <137430320+AlbertoRubalcava@users.noreply.github.com>
2024-12-10 10:31:50 -0800

    server test possible fix

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -7,11 +7,8 @@ import '../css/navbar.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';        
-// import Course from './components/courses';
-// import Home from './components/home';
-
-const Home = () => <div>Home Component</div>;
-const Course = () => <div>Course Component</div>;
+import Course from './components/courses';
+import Home from './components/home';
 
 const App = () => {
     return (
