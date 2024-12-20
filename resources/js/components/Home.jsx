0bc12622202a603efe4b7229f6dc485f78038aaa
alberto rubalcava <137430320+AlbertoRubalcava@users.noreply.github.com>
2024-12-20 15:38:41 -0800

    added bootstrap to homepage
    more accessible to all screen sizes

--- resources/js/components/Home.jsx
+++ resources/js/components/Home.jsx
@@ -24,44 +24,58 @@ const Home = () => {
             setErrorMessage('Graduation Semester is a required field');
             return;
         }
-        // Pass selectedSemester to the next page
         navigate('/recommendation', { state: { selectedSemester } });
     };
+
     return (
-        <>
-            <div className="hero-section">
-                <img src={baseUrl + Library} alt="Library" className="hero-image" />
-                <div className="welcome-text">
-                    <h1>Welcome, [Name]!</h1>
-                    <p>Plan your degree and take a step towards your success.</p>
-                    <p><strong>Major:</strong> [Mathematics]</p>
-                    <p><strong>GPA:</strong> [3.6]</p>
+        <div className="container">
+            <div className="row hero-section">
+                <div className="col-12">
+                    <img src={baseUrl + Library} alt="Library" className="hero-image" />
+                    <div className="welcome-text">
+                        <h1>Welcome, [Name]!</h1>
+                        <p>Plan your degree and take a step towards your success.</p>
+                        <p><strong>Major:</strong> [Mathematics]</p>
+                        <p><strong>GPA:</strong> [3.6]</p>
+                    </div>
                 </div>
             </div>
-            <div className="home-box">
-                <div className="course-roadmap">
-                    <h2>Course Roadmap Recommendation</h2>
-                    <label htmlFor="graduation-semester">Desired Graduation Semester<p id="red">*</p></label>
-                    <select id="graduation-semester" value={selectedSemester} onChange={handleDropdownChange}>
-                        <option value="">Select</option>
-                        <option value="Fall 2025">Fall 2025</option>
-                        <option value="Spring 2026">Spring 2026</option>
-                        <option value="Fall 2026">Fall 2026</option>
-                        <option value="Spring 2027">Spring 2027</option>
-                        <option value="Fall 2027">Fall 2027</option>
-                        <option value="Spring 2028">Spring 2028</option>
-                    </select>
-                    {errorMessage && <span className='errormessage'>{errorMessage}</span>}
-                    <button className="generate-button" onClick={generateClick}>Generate my Roadmap</button>
-                    
-                    <p className="disclaimer">
-                        *This is software uses  
-                        <a href="https://www.csun.edu/it/academic-technology/artificial-intelligence-ai" className="disclaimer-link">AI</a>
-                        . Please check all recommendations.
-                    </p>
+            <div className="row home-box">
+                <div className="col-12">
+                    <div className="course-roadmap">
+                        <h2>Course Roadmap Recommendation</h2>
+                        <label htmlFor="graduation-semester">
+                            Desired Graduation Semester
+                            <p id="red">*</p>
+                        </label>
+                        <select
+                            id="graduation-semester"
+                            value={selectedSemester}
+                            onChange={handleDropdownChange}
+                        >
+                            <option value="">Select</option>
+                            <option value="Fall 2025">Fall 2025</option>
+                            <option value="Spring 2026">Spring 2026</option>
+                            <option value="Fall 2026">Fall 2026</option>
+                            <option value="Spring 2027">Spring 2027</option>
+                            <option value="Fall 2027">Fall 2027</option>
+                            <option value="Spring 2028">Spring 2028</option>
+                        </select>
+                        {errorMessage && (
+                            <span className="errormessage">{errorMessage}</span>
+                        )}
+                        <button className="generate-button" onClick={generateClick}>
+                            Generate my Roadmap
+                        </button>
+                        <p className="disclaimer">*This software uses
+                            <a href="https://www.csun.edu/it/academic-technology/artificial-intelligence-ai"
+                                className="disclaimer-link">
+                                AI
+                            </a>. Please check all recommendations.</p>
+                    </div>
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
